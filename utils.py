7414eb4be8fc64a105367172afbdd59c1843325e
root <jli2274@wisc.edu>
2022-03-18 07:27:43 +0000

    utils_update

--- utils.py
+++ utils.py
@@ -5,25 +5,25 @@ def readGeneList(file):
     flag = 0
     line = f.readline()
     while line :
-      line = f.readline()
       if re.search('>', line):
           flag = 1
+          line = f.readline()
           continue
       else:
         if flag == 0 :
+            line = f.readline()
             continue
         gene = line.split('\n')
         gene = gene[0]
         ls.append(gene)
-        line = f.readline()
+      line = f.readline()
   f.close()
   return ls
 
-def writeGeneList(geneList, file, description):
-    list_name = f'{geneList=}'.split('=')[0]
-    with open(file, 'w') as f:
-        print(description, file = f)
-        print(f'> {list_name}', file = f)
-        for i in geneList:
-            print(i, file = f)
-    f.close()
+def writeGeneList(geneList, file, description, list_name):
+  with open(file, 'w') as f:
+    print(description, file = f)
+    print(f'> {list_name}', file = f)
+    for i in geneList:
+        print(i, file = f)
+  f.close()
